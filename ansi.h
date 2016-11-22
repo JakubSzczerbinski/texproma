fcb1abe9b3aeb4bd36a7426c1b397b07f3fefe9c
Krystian Bacławski <krystian.baclawski@gmail.com>
2016-11-22 19:47:55 +0100

    Do not display ANSI codes in windows console.

--- ansi.h
+++ ansi.h
@@ -1,6 +1,8 @@
 #ifndef __ANSI_H__
 #define __ANSI_H__
 
+#ifndef __MINGW32__
+
 #define CSI     "\033["
 
 #define RESET           CSI "0m"
@@ -14,5 +16,21 @@
 #define MAGENTA CSI "35m"
 #define CYAN    CSI "36m"
 #define WHITE   CSI "37m"
+
+#else
+
+#define RESET ""
+#define BOLD ""
+#define UNDERLINE ""
+
+#define RED ""
+#define GREEN ""
+#define YELLOW ""
+#define BLUE ""
+#define MAGENTA ""
+#define CYAN ""
+#define WHITE ""
+
+#endif
 
 #endif
